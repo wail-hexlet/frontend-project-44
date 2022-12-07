6c77f689ba868bf7abcd3be5ef565785137a0f06
Anatoliy Tupitsin <hexlet@wailbox.ru>
2022-12-07 23:01:36 +0300

    + lint and minor changes

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,5 @@
 import readlineSync from 'readline-sync';
 
 export default function getUserInput(question) {
-    return readlineSync.question(question);
+  return readlineSync.question(question);
 }
