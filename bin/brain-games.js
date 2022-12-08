e4001feb051a67d9b0d4c243ba92388a7d144f04
Anatoliy Tupitsin <hexlet@wailbox.ru>
2022-12-09 00:13:05 +0300

    ref

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -3,4 +3,4 @@ import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
-console.log(`Hello ${name}!`);
+console.log(`Hello, ${name}!`);
