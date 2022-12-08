b20f0f685dca2116fe449507938e1c008efa29b8
Anatoliy Tupitsin <hexlet@wailbox.ru>
2022-12-09 00:00:42 +0300

    refactor

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
-import getUserInput from '../src/cli.js';
+import readlineSync from 'readline-sync';
 
 console.log('Welcome to the Brain Games!');
-const name = getUserInput('May I have your name? ');
+const name = readlineSync.question('May I have your name? ');
 console.log(`Hello ${name}!`);
