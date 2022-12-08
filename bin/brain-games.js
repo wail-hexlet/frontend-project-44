466d81393beee3311370a6045525b29e19670334
Anatoliy Tupitsin <hexlet@wailbox.ru>
2022-12-08 23:49:24 +0300

    refactor

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import getUserInput from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
+// console.log('Welcome to the Brain Games!');
 global.UserName = getUserInput('May I have your name? ');
 console.log(`Hello ${global.UserName}!`);
